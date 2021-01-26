4fcd48706bfc391209adc5ef0f775ef698394275
Maksymilian Bolimowski <bolimek69@gmail.com>
2021-01-26 18:09:49 +0100

    Game window init commit

--- PanstwaMiastaQT/main.cpp
+++ PanstwaMiastaQT/main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Login x;
     x.show();
-    //gameMain z;
-    //z.show();
+    gameMain z;
+    z.show();
 
     return a.exec();
 }
